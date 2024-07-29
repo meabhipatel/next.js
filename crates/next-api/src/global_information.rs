d58817f5787432176b6b611e093dd46b37b4ef23
Lichu Acuña <lichu.acuna@vercel.com>
2024-07-29 17:17:38 -0300

    Added a GlobalInformation struct, which is made available through the ChunkingContext trait. Constructing a new GlobalInformation object takes a Project parameter, giving the constructor access to global information

---
+++ crates/next-api/src/global_information.rs
@@ -0,0 +1,15 @@
+use anyhow::Result;
+use turbo_tasks::Vc;
+use turbopack_binding::turbopack::core::chunk::global_information::{
+    GlobalInformation, OptionGlobalInformation,
+};
+
+use crate::project::Project;
+
+#[turbo_tasks::function]
+pub async fn build_global_information(
+    _project: Vc<Project>,
+) -> Result<Vc<OptionGlobalInformation>> {
+    let global_information = GlobalInformation {};
+    Ok(Vc::cell(Some(global_information)))
+}
